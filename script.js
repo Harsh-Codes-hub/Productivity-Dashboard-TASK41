f5e20209ff75f1e80573c8cf758fc16b10150536
Harsh Gajrani <harshgajrani23@gmail.com>
2026-06-22 12:38:04 +0530

    refactor theme handling and update button color for improved visibility

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ const ProductivityDashboard = {
 
   state: {
     currentTheme: 0,
+
+    weatherData: null,
+
+    location: {
+      latitude: null,
+      longitude: null,
+    },
   },
 
   // =========================
@@ -25,8 +32,27 @@ const ProductivityDashboard = {
   config: {
     themes: ["theme-1", "theme-2", "theme-3", "theme-4", "theme-5"],
 
+    weather: {
+      apiKey: "apiKey",
+      apiUrl: "https://api.openweathermap.org/data/2.5/weather",
+    },
+
     storageKeys: {
       theme: "productivity-theme",
+    },
+  },
+
+  // =========================
+  // Storage
+  // =========================
+
+  storage: {
+    saveData(key, value) {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+
+    getData(key) {
+      return JSON.parse(localStorage.getItem(key));
     },
   },
 
@@ -50,6 +76,77 @@ const ProductivityDashboard = {
       const nextTheme = themes[ProductivityDashboard.state.currentTheme];
 
       html.classList.add(nextTheme);
+
+      ProductivityDashboard.storage.saveData(
+        ProductivityDashboard.config.storageKeys.theme,
+        ProductivityDashboard.state.currentTheme,
+      );
+    },
+
+    loadTheme() {
+      const savedTheme = ProductivityDashboard.storage.getData(
+        ProductivityDashboard.config.storageKeys.theme,
+      );
+
+      if (savedTheme === null) return;
+
+      ProductivityDashboard.state.currentTheme = savedTheme;
+
+      ProductivityDashboard.elements.html.classList.remove(
+        ...ProductivityDashboard.config.themes,
+      );
+
+      ProductivityDashboard.elements.html.classList.add(
+        ProductivityDashboard.config.themes[savedTheme],
+      );
+    },
+  },
+
+  // =========================
+  // Weather Logic
+  // =========================
+
+  weather: {
+    async fetchWeather() {
+      try {
+        const latitude = ProductivityDashboard.state.location.latitude;
+
+        const longitude = ProductivityDashboard.state.location.longitude;
+
+        const apiKey = ProductivityDashboard.config.weather.apiKey;
+        console.log(apiKey.length);
+
+        const url = `${ProductivityDashboard.config.weather.apiUrl}?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+        console.log(url);
+
+        const response = await fetch(url);
+        console.log(response);
+
+        const data = await response.json();
+
+        ProductivityDashboard.state.weatherData = data;
+
+        console.log(ProductivityDashboard.state.weatherData);
+      } catch (error) {
+        console.error("Weather Fetch Error:", error);
+      }
+    },
+
+    getLocation() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        ProductivityDashboard.state.location.latitude =
+          position.coords.latitude;
+
+        ProductivityDashboard.state.location.longitude =
+          position.coords.longitude;
+
+        console.log(ProductivityDashboard.state.location);
+
+        ProductivityDashboard.weather.fetchWeather();
+      });
+    },
+    updateBanner() {
+      console.log("Updating Banner...");
     },
   },
 
@@ -79,6 +176,10 @@ const ProductivityDashboard = {
 
   init() {
     this.cacheElements();
+
+    this.theme.loadTheme();
+
+    this.weather.getLocation();
 
     this.bindEvents();
   },
